27cb013ec4e52a1b4f1ea6248cb646ab5e855cdb
Kidongg <dlrlehd7@daum.net>
2024-03-23 15:44:37 +0900

    수정, 삭제 버튼

--- app/components/table/columns.tsx
+++ app/components/table/columns.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { ColumnDef } from "@tanstack/react-table";
+import { MdEditSquare } from "react-icons/md";
+import { MdDelete } from "react-icons/md";
 
 export type Payment = {
   id: string;
@@ -21,5 +23,18 @@ export const paymentColumns: ColumnDef<Payment>[] = [
   {
     accessorKey: "email",
     header: "이메일",
+  },
+  {
+    id: "edit",
+    header: "수정",
+    cell: ({ row }) => {
+      return <MdEditSquare className="cursor-pointer" />;
+    },
+  },
+  {
+    id: "delete",
+    cell: ({ row }) => {
+      return <MdDelete className="cursor-pointer" />;
+    },
   },
 ];
