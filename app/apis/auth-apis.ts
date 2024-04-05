742864bd6fb444299db117f0fc097e0235aad9b7
invigoworks_frontend <invigoworks_frontend@invigoworks-frontendui-MacBookPro.local>
2024-04-05 19:03:33 +0900

    리프레시토큰 작업 중 - 빨간 주석 참고

--- app/apis/auth-apis.ts
+++ app/apis/auth-apis.ts
@@ -1,6 +1,10 @@
-import { getAccessToken } from "../utils/utils";
+import {
+  getAccessToken,
+  getRefreshToken,
+  isAccessTokenExpired,
+} from "../utils/utils";
 
-// 로그인 API 함수
+// 로그인(토큰 발급) API 함수
 export const login = async (req: LoginType) => {
   console.log("API to loginData: ", req);
 
@@ -18,24 +22,60 @@ export const login = async (req: LoginType) => {
   return res;
 };
 
-// 유저 정보 조회 API 함수
-export const getUserInfo = async () => {
+// 토큰 갱신 API 함수
+export const updateToken = async () => {
   const accessToken = getAccessToken();
+  const refreshToken = getRefreshToken();
 
-  const res = await fetch(`${process.env.NEXT_PUBLIC_DEV_API}/api/user`, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${accessToken}`,
-    },
-  });
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_DEV_API}/api/refresh-access-token`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify({ refreshToken }),
+    }
+  );
 
   if (!res.ok) {
     throw new Error(`HTTP error! status: ${res.status}`);
   }
 
+  //! 쿠키에 저장된 토큰 삭제하는 로직 추가
+  //! 헤더에서 새로운 토큰 받아와서 쿠키에 저장하는 로직 추가
+  //! API를 재호출하는 로딕 추가
+
   const data = await res.json();
   return data;
+};
+
+// 유저 정보 조회 API 함수
+export const getUserInfo = async () => {
+  const accessToken = getAccessToken();
+
+  if (isAccessTokenExpired(accessToken)) {
+    // 토큰이 만료됐을 때
+    updateToken();
+    return Promise.reject("Token is expired");
+  } else {
+    // 토큰이 만료되지 않았을 때
+    const res = await fetch(`${process.env.NEXT_PUBLIC_DEV_API}/api/user`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    if (!res.ok) {
+      throw new Error(`HTTP error! status: ${res.status}`);
+    }
+
+    const data = await res.json();
+    return data;
+  }
 };
 
 //! 타입 정의
--- app/utils/utils.ts
+++ app/utils/utils.ts
@@ -2,14 +2,33 @@ import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 // 테일윈드 클래스와 병합하는 clsx 래퍼 함수
-export function cn(...inputs: ClassValue[]) {
+export const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs));
-}
+};
 
 // Access Token을 가져오는 함수
-export function getAccessToken() {
+export const getAccessToken = () => {
   return document.cookie
     .split("; ")
     .find((row) => row.startsWith("accessToken="))
     ?.split("=")[1];
-}
+};
+
+// Access Token이 만료됐는지 확인하는 함수
+export const isAccessTokenExpired = (token: string | undefined) => {
+  if (!token) return true;
+
+  const payload = JSON.parse(atob(token.split(".")[1]));
+  const exp = payload.exp * 1000;
+  const now = Date.now();
+
+  return now > exp;
+};
+
+// Refresh Token을 가져오는 함수
+export const getRefreshToken = () => {
+  return document.cookie
+    .split("; ")
+    .find((row) => row.startsWith("refreshToken="))
+    ?.split("=")[1];
+};
